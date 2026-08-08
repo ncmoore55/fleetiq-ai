f812b293a28cb29dc20144d89ee1e74c32c6441d
Nikolas Moore <nikolasmoore2007@gmail.com>
2026-08-08 14:08:43 -0600

    fleat: add mock vehicle search results

--- backend/main.py
+++ backend/main.py
@@ -19,4 +19,23 @@ def home():
 
 @app.post("/search")
 def search_vehicles(search_criteria: dict):
-    return [search_criteria]
+    return [
+        {
+            "make": search_criteria["make"],
+            "model": search_criteria["model"],
+            "year": search_criteria["year"],
+            "maximumMileage": "42000",
+        },
+        {
+            "make": search_criteria["make"],
+            "model": search_criteria["model"],
+            "year": search_criteria["year"],
+            "maximumMileage": "58000",
+        },
+        {
+            "make": search_criteria["make"],
+            "model": search_criteria["model"],
+            "year": search_criteria["year"],
+            "maximumMileage": "71000",
+        },
+    ]
