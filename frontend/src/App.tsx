e86e0fb3df19e647982aaefed149391db9cf5ede
Nikolas Moore <nikolasmoore2007@gmail.com>
2026-08-11 13:57:04 -0600

    refactor: extract vehicle results component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import './App.css'
-import VehicleCard from "./components/VehicleCard"
 import type { Vehicle, VehicleSearchCriteria } from "./types/vehicle"
 import { searchVehicles } from "./services/vehicleService"
 import VehicleSearchForm from "./components/VehicleSearchForm"
+import VehicleResults from "./components/VehicleResults"
 
 // main applicaion components
 function App() {
@@ -30,32 +30,10 @@ return (
 
 <VehicleSearchForm onSearch={handleSearch} />
 
-{/* Displays each vehicle returned by the backend */}
-{searchResults.length > 0 && (
-
-  <div>
-    <h2>Search Results</h2>
-    <p>
-      {searchResults.length} {searchResults.length == 1 ? "vehicle" : "vehicles"} found
-    </p>
-
-    {searchResults.map((vehicle, index) => (
-      <VehicleCard
-       key={index}
-       year={vehicle.year}
-       make={vehicle.make}
-       model={vehicle.model}
-       maximumMileage={vehicle.maximumMileage}
-      />
-    ))}
-  </div>
-)}
-
-{/* Displays a message when no vehicles are found */}
-{hasSearched && searchResults.length === 0 && (
-  <p>No vehicles found matching your search.</p>
-)}
-
+<VehicleResults
+  vehicles={searchResults}
+  hasSearched={hasSearched}
+/>  
   </div>
   )
 }
